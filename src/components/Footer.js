20e68ff605e158bff50b0e3badadbc882e86cda9
Mickey <poverwhelming@gmail.com>
2019-07-30 16:34:10 +0800

    updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
           const { style, icon, name, url } = social;
           return (
             <li key={url}>
-              <a href={url} className={`icon ${style} ${icon}`}>
+              <a href={url} className={`icon ${style} ${icon}`} target="_blank">
                 <span className="label">{name}</span>
               </a>
             </li>
@@ -16,9 +16,9 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li>&copy; Spectral</li>
+        <li>&copy; Acid Lab 岸汐職人聚落</li>
         <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
+          Maintain By: <a href="http://rustyfinger.com">Mickey Chen</a>
         </li>
       </ul>
     </footer>
