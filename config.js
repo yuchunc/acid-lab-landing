4de95a8ed5878b5ff1063c616c926210ee50f097
Mickey <poverwhelming@gmail.com>
2019-09-21 15:46:38 +0800

    updated favicon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/favicon.png',
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Acid Lab 岸汐職人聚落',
   subHeading: `
